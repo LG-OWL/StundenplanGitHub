877c569d05504de6895411ad1900d58b8704341f
Lorenz <CallMeEnte@web.de>
2018-07-25 12:44:29 +0200

    Versuch Datenbank mit einzubinden

--- Stundenplan/Stundenplan/Controllers/HomeController.cs
+++ Stundenplan/Stundenplan/Controllers/HomeController.cs
@@ -6,64 +6,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Stundenplan.Models;
 using Stundenplan.ViewModels;
+using Stundenplan.Tests;
 
 namespace Stundenplan.Controllers
 {
     public class HomeController : Controller
     {
+        private StundenplanDbContext _context;
+
+        public HomeController(StundenplanDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             KlasseTestwerteViewModel vm = new KlasseTestwerteViewModel();
-            List<StundenTestwerteViewModel> stundenlist = new List<StundenTestwerteViewModel>();
-            for (int i = 0; i < 5; i++)
-            {
-                Random rnd = new Random();
-                string wochentag;
-                switch (i)
-                {
-                    case 0:
-                        wochentag = "Montag";
-                        break;
-                    case 1:
-                        wochentag = "Dienstag";
-                        break;
-                    case 2:
-                        wochentag = "Mittwoch";
-                        break;
-                    case 3:
-                        wochentag = "Donnerstag";
-                        break;
-                    case 4:
-                        wochentag = "Freitag";
-                        break;
-                    default:
-                        wochentag = "";
-                        break;
-                }
-                for (int j = 1; j <= 6; j++)
-                {
-                    int lessonDecision = rnd.Next(1, 4);
-                    string lessonStr;
-                    switch (lessonDecision)
-                    {
-                        case 1:
-                            lessonStr = "Mathe";
-                            break;
-                        case 2:
-                            lessonStr = "Deutsch";
-                            break;
-                        case 3:
-                            lessonStr = "Englisch";
-                            break;
-                        default:
-                            lessonStr = "Frei";
-                            break;
-                    }
-                    StundenTestwerteViewModel stunden = new StundenTestwerteViewModel() { Stunde = j, Wochentag = wochentag, Fach = lessonStr };
-                    stundenlist.Add(stunden);
-                }
-            }
-            vm.Stunden = stundenlist;
+            //vm.Stunden = Testing.CreateTestValues();
+            vm.Stunden = _context.Klasse.FirstOrDefault(k => k.Id == 1).Stundens;
             return View(vm);
         }
 
---
+++ Stundenplan/Stundenplan/Tests/Testing.cs
@@ -0,0 +1,65 @@
+﻿using Stundenplan.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stundenplan.Tests
+{
+    public class Testing
+    {
+        public static List<StundenTestwerteViewModel> CreateTestValues()
+        {
+            List<StundenTestwerteViewModel> stundenlist = new List<StundenTestwerteViewModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                Random rnd = new Random();
+                string wochentag;
+                switch (i)
+                {
+                    case 0:
+                        wochentag = "Montag";
+                        break;
+                    case 1:
+                        wochentag = "Dienstag";
+                        break;
+                    case 2:
+                        wochentag = "Mittwoch";
+                        break;
+                    case 3:
+                        wochentag = "Donnerstag";
+                        break;
+                    case 4:
+                        wochentag = "Freitag";
+                        break;
+                    default:
+                        wochentag = "";
+                        break;
+                }
+                for (int j = 1; j <= 6; j++)
+                {
+                    int lessonDecision = rnd.Next(1, 4);
+                    string lessonStr;
+                    switch (lessonDecision)
+                    {
+                        case 1:
+                            lessonStr = "Mathe";
+                            break;
+                        case 2:
+                            lessonStr = "Deutsch";
+                            break;
+                        case 3:
+                            lessonStr = "Englisch";
+                            break;
+                        default:
+                            lessonStr = "Frei";
+                            break;
+                    }
+                    StundenTestwerteViewModel stunden = new StundenTestwerteViewModel() { Stunde = j, Wochentag = wochentag, Fach = lessonStr };
+                    stundenlist.Add(stunden);
+                }
+            }
+            return stundenlist;
+        }
+    }
+}
